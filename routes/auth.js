baaffd9392958f7132e023e98b74d04a04861c67
nstruds <nicolai.xs@gmail.com>
2016-02-22 13:35:25 +0100

    Added some routesfor login and register

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/register', function(req, res, next) {
+  res.send('here you register');
+});
+
+router.post('/register', function(req, res, next) {
+    var username = req.body['username'];
+  res.send('Hello ' + username + '!');
+});
+
+router.get('/login', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
