16ee103e51068c93306dc52e816e2b69078763f1
nstruds <nicolai.xs@gmail.com>
2016-02-22 15:49:32 +0100

    added bootsnipp menu and articles

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mandatory Assignment - Frameworks 2016' });
+  res.render('index', { title: 'Frontpage' });
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
+});
+
+router.get('/articles', function(req, res, next) {
+  res.render('articles', { title: 'Articles' });
 });
 
 module.exports = router;
